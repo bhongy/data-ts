9f3ba2fb9b0fcad7991543857c2a7cde21fc77f7
Thanik Bhongbhibhat <email@thanik.me>
2019-03-28 19:00:27 -0700

    Implement "map" for both Nothing and Just- ap and chain doens't work

--- src/LazyMaybe.test.ts
+++ src/LazyMaybe.test.ts
@@ -1,20 +1,21 @@
-import { just } from './LazyMaybe';
+import { Fn } from './utils';
+import { Maybe, just } from './LazyMaybe';
 
 describe('LazyMaybe', () => {
+  const f: Fn<number, number> = jest.fn((x: number) => x * 10);
+  const g: Fn<number, Maybe<number>> = jest.fn((x: number) => just(x / 10));
+
   test('... lazy ...', () => {
-    const f = jest.fn((x: number) => x * 10);
-    const g = jest.fn((x: number) => just(x / 10));
     const u = just(1)
       .map(f)
-      // .chain(g)
+      .chain(g)
       .map(f);
 
     expect(f).not.toHaveBeenCalled();
-    // expect(g).not.toHaveBeenCalled();
+    expect(g).not.toHaveBeenCalled();
 
-    const y = u.fold(x => x + 5);
-    // expect(y).toBe(15);
-    expect(y).toBe(105);
+    const y = u.fold(() => null, (x: number) => x + 5);
+    expect(y).toBe(15);
     expect(f).toHaveBeenCalledTimes(2);
   });
 });
--- src/LazyMaybe.ts
+++ src/LazyMaybe.ts
@@ -1,19 +1,100 @@
-import { Lazy, compose, constant } from './utils';
-import * as Functor from './Functor';
+/**
+ * Use when the cause of error does not need to be communicated.
+ */
 
-class Just<T> implements Functor.Interface<T> {
-  constructor(private readonly x: Lazy<T>) {}
+import { Lazy, identity } from './utils';
+import * as Monad from './Monad';
 
-  map<U>(f: (x: T) => U): Maybe<U> {
-    // cannot use `constant` here because we don't have value `x`
-    return new Just(() => f(this.x()));
+/** Internal implementation. Do not export. */
+
+// consumers never works with the contained value directly
+const $thunk = Symbol('Maybe.thunk');
+const $id = Symbol('Maybe.id');
+const $Nothing = Symbol('Maybe.Nothing');
+const $Just = Symbol('Maybe.Just');
+
+class Nothing<T> implements Monad.Interface<T> {
+  private readonly [$id] = $Nothing;
+
+  fold<L, U>(f: () => L, g: (x: T) => U): L {
+    return f();
   }
 
-  fold<U>(f: (x: T) => U): U {
-    return f(this.x());
+  map<U>(f: (x: T) => U): Maybe<U> {
+    return nothing;
+  }
+
+  ap<A, B>(this: Maybe<(a: A) => B>, other: Maybe<A>): Maybe<B> {
+    return nothing;
+  }
+
+  chain<U>(f: (x: T) => Maybe<U>): Maybe<U> {
+    return nothing;
+  }
+
+  toString(): string {
+    return 'Nothing';
+  }
+
+  inspect() {
+    return this.toString();
   }
 }
 
-export type Maybe<T> = Just<T>;
-// using `constant` here is fine because `x` is already a value
-export const just = <T>(x: T): Maybe<T> => new Just(constant(x));
+class Just<T> implements Monad.Interface<T> {
+  private readonly [$id] = $Just;
+  private readonly [$thunk]: Lazy<T>;
+
+  constructor(thunk: Lazy<T>) {
+    this[$thunk] = thunk;
+  }
+
+  fold<L, U>(f: () => L, g: (x: T) => U): U {
+    return g(this[$thunk]());
+  }
+
+  // map :: Maybe a ~> (a -> b) -> Maybe b
+  map<U>(f: (x: T) => U): Maybe<U> {
+    return new Just(() => f(this[$thunk]()));
+    // this is better but need to fix compose (it doesn't infer type correctly)
+    // return new Just(compose(f, this[$thunk]));
+    // return this.chain(x => just(f(x)));
+  }
+
+  // T is (a: A) => B
+  // ap :: Maybe (a -> b) ~> Maybe a -> Maybe b
+  ap<A, B>(this: Maybe<(a: A) => B>, other: Maybe<A>): Maybe<B> {
+    throw new NotImplementedError('ap');
+    // return this.chain(f => other.map(f));
+  }
+
+  // chain :: Maybe a ~> (a -> Maybe b) -> Maybe b
+  chain<U>(f: (x: T) => Maybe<U>): Maybe<U> {
+    throw new NotImplementedError('chain');
+  }
+
+  // this "runs" all "queued" functions
+  toString(): string {
+    return `Just ${this[$thunk]()}`;
+  }
+
+  inspect() {
+    return this.toString();
+  }
+}
+
+export type Maybe<T> = Nothing<T> | Just<T>;
+export const nothing: Maybe<never> = new Nothing();
+export const just = <T>(x: T): Maybe<T> => new Just(() => x);
+
+export const of = just;
+// export const empty = nothing;
+
+export const fromNullable = <T>(x: undefined | null | T): Maybe<T> =>
+  x == null ? nothing : just(x);
+
+class NotImplementedError extends Error {
+  constructor(name: string) {
+    super(`"${name}" is not implemented`);
+  }
+}
